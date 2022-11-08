511f7851e2d9da541af557e2fd7dd169ea615275
Jonatan <intartif@gmail.com>
2022-11-08 12:03:19 -0500

    (MPP): Subiendo model, servicio y controller para auth

--- mi-primer-proyecto/src/controllers/items.ts
+++ mi-primer-proyecto/src/controllers/items.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { insertCar, getCar } from "../services/item";
+import { insertCar, getCar, getCars, updateCar, deleteCar } from "../services/item";
 import { handleHttp } from "../utils/error.handler";
 
 const getItem = async ({ params }: Request, res: Response) => {
@@ -13,17 +13,20 @@ const getItem = async ({ params }: Request, res: Response) => {
   }
 };
 
-const getItems = ({ body }: Request, res: Response) => {
+const getItems = async (req: Request, res: Response) => {
   try {
-
+    const response = await getCars();
+    res.send(response);
   } catch (e) {
     handleHttp(res, "ERROR_GET_ITEMS", e);
   }
 };
 
-const updateItem = ({ body }: Request, res: Response) => {
+const updateItem = async ({ params, body }: Request, res: Response) => {
   try {
-
+    const { id } = params;
+    const response = await updateCar(id, body);
+    res.send(response);
   } catch (e) {
     handleHttp(res, "ERROR_UPDATE_ITEM", e);
   }
@@ -38,9 +41,11 @@ const addItem = async ({ body }: Request, res: Response) => {
   }
 };
 
-const deleteItem = ({ body }: Request, res: Response) => {
+const deleteItem = async ({ params }: Request, res: Response) => {
   try {
-
+    const { id } = params;
+    const response = await deleteCar(id);
+    res.send(response);
   } catch (e) {
     handleHttp(res, "ERROR_DELETE_ITEM", e);
   }
--- mi-primer-proyecto/src/services/item.ts
+++ mi-primer-proyecto/src/services/item.ts
@@ -1,6 +1,11 @@
 import { Car } from "../interfaces/car.interface";
 import ItemModel from "../models/item";
 
+const getCars = async () => {
+  const responseItem = await ItemModel.find({});
+  return responseItem;
+};
+
 const insertCar = async (item: Car) => {
   const responseInsert = await ItemModel.create(item);
   return responseInsert;
@@ -11,4 +16,16 @@ const getCar = async (id: string) => {
   return responseItem;
 };
 
-export { insertCar, getCar };
+const updateCar = async (id: string, data: Car) => {
+  const responseItem = await ItemModel.findOneAndUpdate({ _id: id }, data, {
+    new: true,
+  });
+  return responseItem;
+};
+
+const deleteCar = async (id: string) => {
+  const responseItem = await ItemModel.remove({ _id: id });
+  return responseItem;
+};
+
+export { insertCar, getCars, getCar, updateCar, deleteCar };
